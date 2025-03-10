1199637d12a8ea4eef51ef650d7f1176a66483f9
Isimsbt <SALMABAATOUR@GMAIL.COM>
2025-03-10 01:54:28 +0100

    Mise à jour de app.py

--- app.py
+++ app.py
@@ -147,4 +147,4 @@ if __name__ == "__main__":
         test_webcam()
     else:
         # Lancement de l'API en local
-        uvicorn.run(app, host="127.0.0.1", port=8000)
+                uvicorn.run(app, host="localhost", port=8000)
